6e6c0b18c15483f6384d6ede777093c7b723d777
Léo Moraes <leohmoraes@users.noreply.github.com>
2016-05-20 16:30:01 -0300

    somente Nginx, Mysql Remoto !

--- Azkfile.js
+++ Azkfile.js
@@ -5,7 +5,7 @@
 systems({
   'inservice': {
     // Dependent systems
-    depends: ['mysql57'],
+    depends: [],
     // More images:  http://images.azk.io
     image: {"docker": "azukiapp/php-fpm:5.6"},
     provision: [
@@ -35,41 +35,6 @@ systems({
     envs: {
       // set instances variables
       APP_DIR: "/azk/#{manifest.dir}",
-    },
-  },
-  mysql57: {
-    // more info about mysql image: http://images.azk.io/#/mysql?from=docs-full_example
-    //docker run --name mysql-server -d -p 3306:3306 azukiapp/mysql:5.7
-    image: {"docker": "azukiapp/mysql:5.7"},
-    shell: "/bin/bash",
-    wait: 25,
-    mounts: {
-      '/var/lib/mysql': persistent("mysql_data"),
-      // to clean mysql data, run:
-      // $ azk shell mysql -c "rm -rf /var/lib/mysql/*"
-    },
-    ports: {
-      // exports global variables: "#{net.port.data}"
-      data: "3306:3306/tcp",
-    },
-    envs: {
-      // SET INSTANCES VARIABLES
-      MYSQL_USER         : "azk",
-      MYSQL_PASSWORD     : "azk",
-      MYSQL_DATABASE     : "#{system.name}_dev", //inservice_dev
-      MYSQL_ROOT_PASSWORD: "azk",
-      //    PMA_URI: "#{system.name}.#{azk.default_domain}",
-
-      //my-secret-pw
-    },
-    export_envs: {
-      MYSQL_USER    : "#{envs.MYSQL_USER}",
-      MYSQL_PASSWORD: "#{envs.MYSQL_PASSWORD}",
-      MYSQL_HOST    : "#{net.host}",
-      MYSQL_PORT    : "#{net.port.data}",
-      MYSQL_DATABASE: "#{envs.MYSQL_DATABASE}"
-
-      //phpmyadmin DATABASE_URL: "mysql2://#{envs.MYSQL_USER}:#{envs.MYSQL_PASSWORD}@#{net.host}:#{net.port.data}/#{envs.MYSQL_DATABASE}",
     },
   },
 });
